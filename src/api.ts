48c20a80383a39ca25f829adff0324b93f2890a8
MrPython <2789762371@qq.com>
2023-12-09 21:07:54 +0800

    Add: 在完成度 > 80% 时采用顺序遍历而非随机撒点的形式，增加效率。

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import qs from 'qs';
-import { WebSocket } from 'ws';
+import { EventEmitter, WebSocket } from 'ws';
 import { sleep } from '.';
 
 axios.defaults.baseURL = "https://www.oi-search.com/paintboard/";
@@ -16,7 +16,7 @@ export type Color = [number, number, number];
 export function init(mirror: string) {
     axios.defaults.baseURL = mirror;
 }
-export async function draw(x: number, y: number, color: Color, uid: number, token: string) {
+export async function draw(x: number, y: number, color: Color, uid: string, token: string) {
     let s = qs.stringify({
         x, y,
         color: toByteStr(color[0]) + toByteStr(color[1]) + toByteStr(color[2]),
@@ -24,10 +24,12 @@ export async function draw(x: number, y: number, color: Color, uid: number, toke
     });
     try {
         let res = await axios.post("/paint", s);
-        if (res.data?.status != 200) throw "返回错误：" + res.data?.data;
+        let header=res.headers.date;
+        console.log(header,s);
+        if (res.data?.status != 200) throw `At ${header} 返回错误：` + res.data?.data;
     } catch (err) {
+        console.error("绘画时发生错误：", err);
         await sleep(5000);
-        console.error("绘画时发生错误：",err);
         return await draw(x, y, color, uid, token);
     }
 }
@@ -37,7 +39,7 @@ export async function getpaintboard() {
         let res = (await axios.get("/board")).data as string;
         var arr: Color[][] = Array.from({ length: 1000 }, () => new Array(600).fill(undefined));
         for (let i = 0; i < 1000; ++i) for (let j = 0; j < 600; ++j) {
-            let p = i * 1000 + i + j;
+            let p = i*3600+i+j*6;
             arr[i][j] = [
                 parseInt(res.substring(p + 0, p + 2), 16),
                 parseInt(res.substring(p + 2, p + 4), 16),
@@ -46,30 +48,28 @@ export async function getpaintboard() {
         }
         return arr;
     } catch (err) {
-        console.error("获取画板时发生错误：",err);
-        await sleep(5000);
+        console.error("获取画板时发生错误：", err);
+        await sleep(1000);
         return await getpaintboard();
     }
 }
 
-export async function websocket(paintboard: Color[][]) {
+export async function websocket(paintboard: Color[][], update: () => void, event: EventEmitter) {
     try {
         let ws = new WebSocket("wss://www.oi-search.com:11451/paintboard/ws");
         ws.on("message", (data: Buffer) => {
             if (data[0] == 0xfa)
-                paintboard[data[2] * 0xff + data[1]][data[4] * 0xff + data[3]] = [data[5], data[6], data[7]];
-            else if (data[0] == 0xfd) (async () => {
-                let p = await getpaintboard();
-                for (let i = 0; i < 1000; ++i) paintboard[i] = p[i];
-            })();
+                paintboard[data[2] * 0xff + data[1]][data[4] * 0xff + data[3]] = [data[5], data[6], data[7]],
+                    event.emit("change", { x: data[2] * 0xff + data[1], y: data[4] * 0xff + data[3] });
+            else if (data[0] == 0xfd) (async () => update())();
         });
         ws.on("close", () => {
             console.error("Websocket 连接断开");
-            websocket(paintboard);
+            websocket(paintboard, update, event);
         });
     } catch (err) {
-        console.error("建立 websocket 连接时发生错误：",err);
-        await sleep(5000);
-        return await websocket(paintboard);
+        console.error("建立 websocket 连接时发生错误：", err);
+        await sleep(1000);
+        return await websocket(paintboard, update, event);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,97 @@
 import { program } from 'commander';
-import { draw, getpaintboard, init, websocket } from './api';
+import { Color, draw, getpaintboard, init, websocket } from './api';
 import Jimp from 'jimp';
+import { EventEmitter } from 'events';
+import { Queue } from './queue';
 
 export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 const thw = false;
 
-export default async function start(image: string, width: string, x: string, y: string, token: string[], mirror?: string) {
+export default async function start(image: string, width: string, sx: string, sy: string, token: string[], mirror?: string) {
+    let x = +sx, y = +sy;
+
     if (mirror) init(mirror);
     let img = await Jimp.read(image);
     img.scale(+width / img.getWidth());
-    let map = await getpaintboard();
-    websocket(map);
+    let map: Color[][] = [];
+
+    let flag = -1;
+    let event = new EventEmitter();
+    const update_map = async () => {
+        map = await getpaintboard();
+        event.emit("update");
+    }, check = () => {
+        if (flag != -1) return;
+        let all = 0, undraw = 0;
+        for (let i = 0; i < img.getWidth(); ++i) for (let j = 0; j < img.getHeight(); ++j) {
+            let color = img.getPixelColor(i, j);
+            let r = color >> 24 & 0xff, g = color >> 16 & 0xff, b = color >> 8 & 0xff, a = (color & 0xff) / 256;
+            if (a <= 0.5) continue;
+            ++all;
+            if (map[x + i][y + j][0] != r || map[x + i][y + j][1] != g || map[x + i][y + j][2] != b) ++undraw;
+        }
+        if (undraw <= all * 0.2) flag = token.length;
+        console.log(`undrawed: ${undraw}/${all} ${undraw/all}`);
+    }
+    await update_map();
+    websocket(map, update_map, event);
     (async () => {
+        check();
         while (true) {
             await sleep(300 * 1000);
-            map = await getpaintboard();
+            update_map();
+            check();
         }
     })();
     for (let i of token) {
-        let p = i.indexOf(':');
+        let p = i.indexOf(' :');
         (async (uid: string, token: string) => {
-            while (true) {
+            while (flag == -1) {
                 let i = Math.floor(Math.random() * img.getWidth()), j = Math.floor(Math.random() * img.getHeight());
                 let color = img.getPixelColor(i, j);
                 let r = color >> 24 & 0xff, g = color >> 16 & 0xff, b = color >> 8 & 0xff, a = (color & 0xff) / 256;
-                if (a > 0.5 && (map[+x + i][+y + j][0] != r || map[+x + i][+y + j][1] != g || map[+x + i][+y + j][2] != b)) {
-                    await draw(+x + i, +y + j, [r, g, b], +uid, token);
-                    await sleep(4990);
+                if (a > 0.5 && (map[x + i][y + j][0] != r || map[x + i][y + j][1] != g || map[x + i][y + j][2] != b)) {
+                    await draw(x + i, y + j, [r, g, b], uid, token);
+                    await sleep(5000);
                 }
-                await sleep(10);
+            }
+            --flag;
+        })(i.substring(0, p), i.substring(p + 1));
+    }
+    while (flag != 0) await sleep(1000);
+    const q = new Queue<{ x: number, y: number }>();
+    const push_queue = (pos: { x: number, y: number }) => {
+        if (pos.x >= x && pos.y >= y && pos.x < x + img.getWidth() && pos.y < y + img.getHeight())
+            q.push(pos);
+        return;
+    };
+    event.on("change", push_queue);
+    event.on("update", () => {
+        q.clear();
+        for (let i = 0; i < img.getWidth(); ++i) for (let j = 0; j < img.getHeight(); ++j) {
+            let color = img.getPixelColor(i, j);
+            let r = color >> 24 & 0xff, g = color >> 16 & 0xff, b = color >> 8 & 0xff, a = (color & 0xff) / 256;
+            if (a <= 0.5) continue;
+            if (map[x + i][y + j][0] != r || map[x + i][y + j][1] != g || map[x + i][y + j][2] != b) push_queue({ x: x + i, y: y + j });
+        }
+    });
+    await update_map();
+    for (let i of token) {
+        let p = i.indexOf(':');
+        await sleep(10);
+        (async (uid: string, token: string) => {
+            while (true) {
+                let p = q.front();
+                if (p) {
+                    q.pop();
+                    let color = img.getPixelColor(p.x - x, p.y - y);
+                    let r = color >> 24 & 0xff, g = color >> 16 & 0xff, b = color >> 8 & 0xff, a = (color & 0xff) / 256;
+                    if (a <= 0.5 || (map[p.x][p.y][0] == r && map[p.x][p.y][1] == g && map[p.x][p.y][2] == b)) continue;
+                    await draw(p.x, p.y, [r, g, b], uid, token);
+                    await sleep(4950);
+                }
+                await sleep(50);
             }
         })(i.substring(0, p), i.substring(p + 1));
     }
